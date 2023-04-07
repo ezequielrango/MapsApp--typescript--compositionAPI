3f2816bde10d15af92bae323481703ad3f2ce22f
Ezequiel Rango <ezequielrango29@gmail.com>
2023-04-07 18:28:19 -0300

    display distance and duration, create tripdetails component

---
+++ src/components/trip-details/TripDetails.vue
@@ -0,0 +1,45 @@
+<template>
+    <div v-if="distance"
+        class="trip-container column">
+        <div class="col">Distancia:  <strong>{{distance}} kms</strong></div>
+        <div class="col">Duración:  <strong>{{duration}} minutos</strong></div>
+
+    </div>
+</template>
+<script lang="ts">
+
+import { useMapStore } from "@/composables/useMapStore";
+import { defineComponent } from "vue";
+
+
+
+export default defineComponent({
+    name: 'TripDetails',
+    setup(){
+
+       const {duration, distance} = useMapStore()
+
+        return{
+            duration,
+            distance
+        }
+    }
+})
+</script>
+
+<style scoped>
+.trip-container{
+    padding: 10px;
+    position: fixed;
+    bottom: 100px;
+    right: 20px;
+    border-radius: 10px;
+    background-color: rgb(210, 221, 115);
+    box-shadow: 0px 10px 10px rgba(0, 0, 0, 0.2);
+    font-size: 20px;
+    width: 350px;
+}
+strong{
+    color: #42b983;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,10 +4,10 @@ import { defineComponent } from 'vue';
 import MapView from '@/components/mapview/MapView.vue';
 import MyLocationBtn from '@/components/my-location-btn/MyLocationBtn.vue';
 import SearchBar from '@/components/searchbar/SearchBar.vue';
-
+import TripDetails from '@/components/trip-details/TripDetails.vue';
 export default defineComponent({
   name: 'HomeView',
-  components: {MapView, MyLocationBtn,SearchBar},
+  components: {MapView, MyLocationBtn,SearchBar, TripDetails},
 
   setup() {
       
@@ -21,5 +21,6 @@ export default defineComponent({
   <MapView/>
   <MyLocationBtn/>
   <SearchBar/>
+  <TripDetails/>
 
 </template>
